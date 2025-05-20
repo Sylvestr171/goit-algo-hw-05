ffb89cfee2ac00f08327431fb7c21c87d5ab9ea5
Oleksii Haiduk <Silvestr171@gmail.com>
2025-05-20 16:21:20 +0300

    add anotation

--- caching_fibonacci.py
+++ caching_fibonacci.py
@@ -1,13 +1,27 @@
 from collections import defaultdict
-from typing import Callable, Dict
+from typing import Callable
 
+# The function for cashing the calculating values of Fibonachi numbers
 def caching_fibonacci() -> Callable:
+    '''
+    The function for cashing the calculating values 
+    of Fibonachi numbers to the dict {key=number of Fibonachi number, value=value of Fibonachi number}
+    
+    return: function
+    
+    '''
     caching_dictionary = defaultdict(int)
     caching_dictionary = {0:0,1:1}
-    print(caching_dictionary)
-    print(caching_dictionary[0])
-    print(caching_dictionary[1])
+    # The function for calculating Fibonachi number
     def fibonachi(number:int) -> int:
+        '''
+        The function of calculating Fibonachi number
+        if it not in cashing dictionary
+        
+        input: int
+        return: int
+        
+        '''
         if number < 0:
             number = 0
         if number in caching_dictionary:
