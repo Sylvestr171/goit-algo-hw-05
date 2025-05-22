f560f1eb1bc3646613d6030f1e4b4b49cf2155fd
Oleksii Haiduk <Silvestr171@gmail.com>
2025-05-22 12:54:40 +0300

    треба відладка

--- log.py
+++ log.py
@@ -1,14 +1,22 @@
-from sys import argv
+from sys import argv, exit
 from pathlib import Path
 from datetime import date, time
-# import re
+from collections import defaultdict
 
+#декоратор для обробки помилок
+def FileNotFoundError_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except FileNotFoundError:
+            return "'FileNotFoundError' Please input correct path"
+    return inner
 
 #функція завантаження логу з файлу
+@FileNotFoundError_error
 def load_logs(file_path: str) -> list[dict]:
-    carrent_dir = Path(__file__).parent
     log_dict_list = []
-    with open(carrent_dir/file_path, "r", encoding="utf-8") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
             for ithem in file.readlines():
                 log_dict_list.append(parse_log_line(ithem))
             return log_dict_list
@@ -29,30 +37,67 @@ def parse_log_line(line: str) -> dict[str, date | time | str]:
 
 #функція фільтрації логів за рівнем
 def filter_logs_by_level(logs: list, level: str) -> list:
-    return filter(lambda x: x['level'] == level, logs)
+    return list(filter(lambda x: x['level'] == level, logs))
 
 
 #функція підрахунку кількості повідомлень за рівнем логування
 def count_logs_by_level(logs: list) -> dict:
-    ...
+    count_dict = defaultdict(int)
+    for line in logs:
+        count_dict[line['level']]+=1
+    return count_dict
 
-#функція підрахунку кількості повідомлень за рівнем логування
+
+#функція для виводу статистики логування
 def display_log_counts(counts: dict):
-    ...
+    print('Рівень логування', 'Кількість', sep='\t|    ')
+    print('------------------------|------------------')
+    for ithem in counts.keys():
+         print (ithem, counts.get(ithem), sep='\t\t\t|\t') 
 
-#декоратор для обробки помилок
-def FileNotFoundError_error(func):
-    def inner(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except FileNotFoundError:
-            return "How can I help you?"
-    return inner
 
-log_dict_list = load_logs('log.txt')
-for i in log_dict_list:
-    print (i)
 
-filter_mesage=filter_logs_by_level(log_dict_list, 'INFO')
-for k in filter_mesage:
-    print (k)
+# log_dict_list = load_logs('log.txt')
+# for i in log_dict_list:
+#     print (i)
+
+# filter_mesage=filter_logs_by_level(log_dict_list, 'INFO')
+# # for k in filter_mesage:
+# #     print (type(k))
+#print(count_logs_by_level(log_dict_list))
+# display_log_counts(count_logs_by_level(log_dict_list))
+
+
+
+if __name__ == '__main__':
+
+    if len(argv) == 2:
+        file_path = str(argv[1])
+        log_dict_list = load_logs(file_path)
+        display_log_counts(count_logs_by_level(log_dict_list))
+    elif len(argv) == 3:
+        file_path = argv[1]
+        level_param = argv[2].upper()
+        log_dict_list = load_logs(file_path)
+        count_dict=count_logs_by_level(log_dict_list)
+        display_log_counts(count_dict)
+        if level_param in count_dict.keys():
+            filter_mesage=filter_logs_by_level(log_dict_list, level_param)
+            print (f'\nДеталі логів для рівня "{level_param}":')
+            for ithem in filter_mesage:
+                print (ithem['date'].isoformat(), ithem['time'].isoformat(), '-', ithem['info'], sep=' ')
+        else:
+            print (f'Incorrect level parameter {level_param}')
+    elif len(argv) == 1:
+        print('Please insert path to file')
+        exit()
+    else:
+        print('More parameters than expected')
+        exit()
+    
+    # log_dict_list = load_logs(file_path)
+    # filter_mesage=filter_logs_by_level(log_dict_list, level_param)
+    # # for k in filter_mesage:
+    # #     print (type(k))
+    # print(count_logs_by_level(log_dict_list))
+    # display_log_counts(count_logs_by_level(log_dict_list))
